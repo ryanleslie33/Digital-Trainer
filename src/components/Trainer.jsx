5beecf8af70a0266ac6c1127ec96955f37e83462
ryan leslie <ryanleslie33@aol.com>
2019-03-01 16:43:56 -0800

    create histroy component and update styling

--- src/components/Trainer.jsx
+++ src/components/Trainer.jsx
@@ -3,12 +3,44 @@ import PropTypes from 'prop-types';
 
 
 let border =  {
-  border: '30px solid black',
+  border: '20px solid black',
   borderRadius: '10px',
+  width:'220px',
+  height:'400px'
+}
+
+let button =  {
+  border: '5px solid black',
+  borderRadius: '10px',
+  width:'210px',
+  height:'50px',
+  padding:'20px',
+  margin:'5px'
 }
 let page = {
   backgroundColor:'grey',
   borderRadius:'30px'
+}
+let header = {
+  width:'160px',
+  border: '5px solid black',
+  borderRadius: '10px',
+  fontSize:'40px',
+  borderRadius:'30px',
+  position:'relative',
+  left:'50px'
+}
+let button2 = {
+  width:'168px',
+  border: '5px solid black',
+  borderRadius: '10px',
+  fontSize:'40px',
+  borderRadius:'30px',
+  position:'relative',
+  left:'50px',
+  height:'54px',
+  backgroundColor:'grey',
+  hover:'20px'
 }
 class Trainer extends React.Component {
 
@@ -27,11 +59,15 @@ class Trainer extends React.Component {
     <br/>       
     <br/> 
     <br/> 
-    <br/> 
-    <br/> 
-    <br/> 
+    <button style={button2} type="text" text='button'>History</button>
+    <p style={header}>Workouts</p>
     <div style={border}>
-    <p style={border}>test</p>
+    <button style={button} type="text" alt="example"/>
+    <button style={button} type="text" placeholder="example"/>
+    <button style={button} type="text" placeholder="example"/>
+    <button style={button} type="text" placeholder="example"/>
+    <button style={button} type="text" placeholder="example"/>
+    <button style={button} type="text" placeholder="example"/>
     </div>
     <br/>       
     <br/> 
@@ -39,12 +75,7 @@ class Trainer extends React.Component {
     <br/> 
     <br/> 
     <br/> 
-    <br/>       
-    <br/> 
-    <br/> 
-    <br/> 
-    <br/> 
-    <br/> 
+    
       </div>
     );
   }
