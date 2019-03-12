d4620861b52a861c471a06fa7aa2b8b4367e79d1
ryan leslie <ryanleslie33@aol.com>
2019-03-12 11:30:26 -0700

    add all imagess to array inside file

--- src/public/images/masterImageList.js
+++ src/public/images/masterImageList.js
@@ -1,5 +1,102 @@
 import reverseGripRow from '../images/3-2b-reverse-grip-bent-over-row.jpg';
+import abCrunch from '../images/ab-crunch.jpg';
+import arnoldPress from '../images/arnold-press.jpg';
+import barbellSquat from '../images/barbell-squat.jpg';
+import benchPress from '../images/bench-press.jpg';
+import bicepCurl from '../images/Bicep-Curl.jpg';
+import dumbbellPress from '../images/bumbell-bench.jpgg';
+import chestSR from '../images/chest-supported-row.jpg';
+import dbRDL from '../images/dumbbell_romanian_deadlift_main.jpg';
+import dbBOR from '../images/dumbbell-bent-over-row.jpg';
+import dbCR from '../images/dumbbell-calf-rasies.jpg';
+import dbFP from '../images/dumbbell-floor-press.jpg';
+import dbFS from '../images/dumbbell-frog-squat.jpg';
+import dbHT from '../images/dumbbell-hip-thrust.jpg';
+import dbLR from '../images/dumbbell-lateral-raise.jpgg';
+import dbS from '../images/dumbbell-squat-4.jpg';
+import dbSLDL from '../images/dumbbell-stiff-leg-deadlift.jpg';
+import dbPO from '../images/Dumbell-Pull-Over.jpg';
+import dbRL from '../images/dumbell-rear-lunge.jpg';
+import gKB from '../images/glute_kickback.jpg';
+import gBG from '../images/glutebridge.jpg';
+import gS from '../images/goblet-squat.jpg';
+import hS from '../images/hack-squat-2.png';
+import hEX from '../images/Hyperextensions.jpg';
+import iBBP from '../images/incline-barbell-press.jpg';
+import iDBP from '../images/incline-dumbbell-bp.jpg';
+import lLC from '../images/laying-leg-curl.jpg';
+import lE from '../images/leg-extensions.jpg';
+import lP from '../images/leg-press.jpg';
+import lIBP from '../images/low-incline-bp.jpg';
+import mP from '../images/machine-press.jpg';
+import oADBR from '../images/One-Arm-Dumbbell-Rows.jpg';
+import pWC from '../images/pin-wheel-curls.jpg';
+import pEW from '../images/plank-exercise-workout.jpg';
+import pU from '../images/push-ups.jpg';
+import rC from '../images/Reverse-Crunches.jpg';
+import rHS from '../images/reverse-hack-squats.jpg';
+import rDL from '../images/romanian-deadlift-exercise.jpg';
+import sLLC from '../images/side-lying-leg-climbers.jpg';
+import sP from '../images/side-plank.jpg';
+import sACO from '../images/single-arm-cross-over.jpg';
+import sS from '../images/sissy-squat.jpg';
+import sU from '../images/sit-up.jpg';
+import sST from '../images/split-squat.jpg';
+import sBC from '../images/Stability-ball-Crunch-for-belly-fat.jpg';
+import sCR from '../images/standing-calf-raise.jpg';
+import sDBP from '../images/Standing-Dumbell-Press.jpg';
+import tDBR from '../images/tripod-dumbbell-row2.jpg';
+import wDBL from '../images/Walking-Dumbbell-Lunge.jpg';
+
 
 export const masterImageList = {
   reverseGripBentOverRow: reverseGripRow
+  abCrunch: abCrunch
+  arnoldPress: arnoldPress
+  barbellSquat: barbellSquat
+  benchPress: benchPress
+  bicepCurl: bicepCurl
+  dumbbellPress: dumbbellPress
+  chestSR: chestSR
+  dbRDL: dbRDL
+  dbBOR: dbBOR
+  dbCR: dbCR
+  dbFP: dbFP
+  dbFS: dbFS
+  dbHT: dbHT
+  dbLR: dbLR
+  dbS: dbS
+  dbSLDL: dbSLDL
+  dbPO: dbPO
+  dbRL: dbRL
+  gKB: gKB
+  gBG: gBG
+  gS: gS
+  hS: hS
+  hEX: hEX
+  iBBP: iBBP
+  lLC: lLC
+  lE: lE
+  lP: lP
+  lIBP: lIBP
+  mP: mP
+  oADBR: oADBR
+  pWC: pWC
+  pEW: pEW
+  pU: pU
+  rC: rC
+  rHS: rHS
+  rDL: rDL
+  sLLC: sLLC
+  sP: sP
+  sACO: sACO
+  sS: sS
+  sU: sU
+  sST: sST
+  sBC: sBC
+  sCR: sCR
+  sDBP: sDBP
+  tDBR: tDBR
+  wDBL: wDBL
+
 }
